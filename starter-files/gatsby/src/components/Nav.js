1977862621c940622e7311e259a8680046db4b5a
Laura Turk <laura.lk.turk@gmail.com>
2020-10-05 10:51:41 -0600

    05 routing

---
+++ starter-files/gatsby/src/components/Nav.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'gatsby'; // named import from gatsby
+// arrow function is also cool
+
+/* navigate from gatsby, allows for imperative {replace: true} adds to browser history */
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Logo</Link>
+        </li>
+        <li>
+          <Link to="/">Hot Now</Link>
+        </li>
+        <li>
+          <Link to="/pizza/">Pizza Menu</Link>
+        </li>
+        <li>
+          <Link to="/slicemasters/">Slicemasters</Link>
+        </li>
+        <li>
+          <Link to="/order/">Order Ahead!</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+/* Link to => is a declarative pattern, 
+imperative is writing out the logic for what will happen when something else happens, 
+like form submit/onclick
+
+button always needs a type */
--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function PizzaPage() {
   return (
     <div>
+      <Nav />
       <p>Hey! I'm the pizza page</p>
     </div>
   );
